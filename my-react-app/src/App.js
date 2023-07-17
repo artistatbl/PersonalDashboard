97d3102d22895ea3647ad3609e129f65cdeb9235
artistatbl <jdaly2991@gmail.com>
2023-07-17 02:59:03 +0100

    expanding new files

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Tabs from './tabs/Tabs'
+import Tabs from './tabs/Tabs';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState('');
+  const [currentDate, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    const updateTimeAndDate = () => {
+      const now = new Date();
+      const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      const date = now.toLocaleDateString([],{day: 'numeric', month: 'long', year: 'numeric' });
+      setCurrentTime(time);
+      setCurrentDate(date);
+    };
+
+    // Update time and date initially
+    updateTimeAndDate();
+
+    // Update time and date every second
+    const intervalId = setInterval(updateTimeAndDate, 1000);
+
+    // Clean up interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className= "app-wrapper">
-    <div className="top-section">
-      <h1>19:50</h1>
-      <h2>1 January 2023</h2>
-    </div>
+    <div className="app-wrapper">
+      <div className="top-section">
+        <h1>{currentTime}</h1>
+        <h2>{currentDate}</h2>
+      </div>
 
+      <div className="content-section"></div>
 
-    <div className="content-section">
-    
-
-    </div>
-
-    <div className="bottom-section">
-      <Tabs />
-
-    </div>
+      <div className="bottom-section">
+        <Tabs />
+      </div>
     </div>
   );
 }
--- my-react-app/src/tabs/Tabs.js
+++ my-react-app/src/tabs/Tabs.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Tabs.css';
 import BookmarksOutlinedIcon from '@mui/icons-material/BookmarksOutlined';
+import PlaylistAddCheckOutlinedIcon from '@mui/icons-material/PlaylistAddCheckOutlined';
+import TextSnippetOutlinedIcon from '@mui/icons-material/TextSnippetOutlined';
 
 function Tabs() {
   return (
     <div className="tabs">
-      <div className="tab">
+      <div className="tab bookmark-tab">
         <div className="tab-title">
           <BookmarksOutlinedIcon />
         </div>
       </div>
-	 <div className="tab">
+	 <div className="tab todo-tab">
         <div className="tab-title">
-          <BookmarksOutlinedIcon />
+          <PlaylistAddCheckOutlinedIcon />
         </div>
       </div>
 
-	 <div className="tab">
+	 <div className="tab note-tab">
         <div className="tab-title">
-          <BookmarksOutlinedIcon />
+          <TextSnippetOutlinedIcon />
         </div>
       </div>
 
